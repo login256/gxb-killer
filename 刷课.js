f3a85f47b89f0f5a30587b11ca99e501218c5ded
login256 <32592096+login256@users.noreply.github.com>
2017-10-08 11:47:33 +0800

    Update and rename 快进、失焦不暂停.js to 刷课.js
    重写了几乎全部代码。
    1、现在可以跳过一切非视频页面了
    2、开始播放声音设置成静音
    3、加速只会被执行一次

---
+++ 刷课.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         高校邦 课程 快进、跳题、跳页、失焦不暂停
+// @namespace    http://tampermonkey.net/
+// @version      1.1.0
+// @description  Please change what to be matched if you want to use this in other pages. Try to take over the world!
+// @author       login256
+// @match        *://buaa.class.gaoxiaobang.com/class/1953*/unit/*/chapter/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	function check()
+	{
+		var videoTest=document.getElementsByClassName("gxb-icon-radio");
+		if(videoTest.length!==0)
+		{
+			for(var i=0;i<videoTest.length;i++)
+			{
+				videoTest[i].click();
+			}
+			if(document.getElementsByClassName('gxb-btn_ submit').length!==0) document.getElementsByClassName('gxb-btn_ submit')[0].click();
+			if(document.getElementsByClassName('gxb-btn_ next').length!==0) document.getElementsByClassName('gxb-btn_ next')[0].click();
+			if(document.getElementsByClassName('gxb-btn_ player').length!==0) document.getElementsByClassName('gxb-btn_ player')[0].click();
+		}
+		//出现视频内测验，由于不用选对，依次点击答案(其实好像根本不用选，但都写了，懒得删），并提交
+		if(document.getElementsByClassName("player-video").length===0)
+		{
+			document.getElementsByClassName("gxb-next-blue")[0].click();
+		}
+		//非课程页面跳过
+	}
+	function speedUp()
+	{
+		if(document.getElementById("vjs_video_3_html5_api")===null)
+		{
+			setTimeout(speedUp,1000);
+		}
+		else
+		{
+			setTimeout(function(){var thv=document.getElementById("vjs_video_3_html5_api");thv.playbackRate=3;thv.muted=true;},0);
+		}
+	}
+	//加速
+	speedUp();
+	window.setInterval(check,2333);
+	window.onblur=function(){setTimeout(function(){if(document.getElementById("vjs_video_3_html5_api")!==null) document.getElementById("vjs_video_3_html5_api").play();},0);};
+	//失焦暂停后重新播放（网站中只是失焦一瞬间会暂停，故直接播放就好）。
+})();
